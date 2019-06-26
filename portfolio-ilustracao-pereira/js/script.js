d58fe770d920e389b9aead224e772bcaeda1a593
Kelly Christina <keely06@live.com>
2019-06-26 11:58:16 -0300

    script fade

--- portfolio-ilustracao-pereira/js/script.js
+++ portfolio-ilustracao-pereira/js/script.js
@@ -1,7 +1,29 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    /*! Fades in page on load */
-    $('body').css('display', 'none');
-    $('body').fadeIn(500);
+//     /*! Fades in page on load */
+//     $('body').css('display', 'none');
+//     $('body').fadeIn(500);
     
-});
+// });
+
+// $('body').css('display', 'none');
+
+$.ajax("index.html")
+.done(function() {
+   $('nav ul li a').click(function() {
+       var href = $(this).attr('href');
+    //    console.log(href);
+
+    $('.container').hide().load(href).fadeIn(1000);
+   })
+})
+
+// $.ajax("index.html")
+// .done(function(){
+//      $('body').hide().fadeIn("1000")
+// }) 
+
+// $.ajax("contato.html")
+// .done(function(){
+//      $('body').hide().fadeIn("1000")
+// }) 

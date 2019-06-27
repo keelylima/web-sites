af8973b1884613163b100b64d4847afdd143bdd7
Kelly Christina <keely06@live.com>
2019-06-27 10:41:07 -0300

    refresh page ajax

--- portfolio-ilustracao-pereira/js/script.js
+++ portfolio-ilustracao-pereira/js/script.js
@@ -1,29 +1,24 @@
-// $(document).ready(function(){
+$("nav ul li a").click(function (event) {
+    event.preventDefault();
+    ajaxPage(event.target.href);
 
-//     /*! Fades in page on load */
-//     $('body').css('display', 'none');
-//     $('body').fadeIn(500);
-    
-// });
+    async function ajaxPage(url) {
+        const pageResponse = await $.ajax(url)
+        const pageText = await pageResponse;
+        replaceContent(pageText);
+    }
 
-// $('body').css('display', 'none');
+    function replaceContent(newText) {
+        const newHtml = document.createElement("div");
+        newHtml.innerHTML = newText;
 
-$.ajax("index.html")
-.done(function() {
-   $('nav ul li a').click(function() {
-       var href = $(this).attr('href');
-    //    console.log(href);
+        const oldContent = document.querySelector(".container-geral");
+        const newContent = newHtml.querySelector(".container-geral");
 
-    $('.container').hide().load(href).fadeIn(1000);
-   })
+        oldContent.innerHTML = newContent.innerHTML;
+    }
+
+    $(".container-geral").hide().fadeIn("slow");
+
+
 })
-
-// $.ajax("index.html")
-// .done(function(){
-//      $('body').hide().fadeIn("1000")
-// }) 
-
-// $.ajax("contato.html")
-// .done(function(){
-//      $('body').hide().fadeIn("1000")
-// }) 
